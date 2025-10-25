75d599b3a3bf34cf55f164300f43d6e8d781eb95
Aleksandr Krivoshein <megalistnn@gmail.com>
2025-10-25 21:53:53 +0400

    feat: implement function 'copy'

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,26 @@
+import { cp, access, mkdir } from "fs/promises";
+
 const copy = async () => {
-  // Write your code here
+  const rootPath = 'src/fs/';
+  const srcDirectory = `${rootPath}files/`;
+  const destDirectory = `${rootPath}files_copy/`;
+  const errorMesage = 'FS operation failed';
+
+  const srcDirectoryExists = await access(srcDirectory)
+    .then(() => true)
+    .catch(() => false);
+
+  const destDirectoryExists = await access(destDirectory)
+    .then(() => true)
+    .catch(() => false);
+
+  if(!srcDirectoryExists || destDirectoryExists) {
+    console.error(errorMesage);
+    return;
+  }
+
+  await cp(srcDirectory, destDirectory, { recursive: true, errorOnExist: true })
+    .catch(() => console.error(errorMesage));
 };
 
 await copy();
