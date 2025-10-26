f0b5af2f3f1cf226f367f47f579aaacd13b1ef46
Aleksandr Krivoshein <megalistnn@gmail.com>
2025-10-26 21:00:02 +0400

    feat: implement function 'calculateHash'

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+
 const calculateHash = async () => {
-  // Write your code here
+  const errorMesage = 'Operation failed';
+  const filePath = 'src/hash/files/fileToCalculateHashFor.txt';
+  const hash = createHash('sha256');
+  const stream = createReadStream(filePath);
+
+  stream.on('data', (chunk) => {
+    hash.update(chunk);
+  });
+  
+  stream.on('end', () => {
+    const fileHash = hash.digest('hex');
+    console.log(fileHash);
+  });
+  
+  stream.on('error', (err) => {
+    console.error(`${errorMesage}: `, err);
+  });
 };
 
 await calculateHash();
